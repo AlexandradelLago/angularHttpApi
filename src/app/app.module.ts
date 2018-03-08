678bf40954a4ec4921fd667ade7a0404d907d94e
akdezgas <andreivelazquez94@gmail.com>
2018-03-08 12:29:15 -0600

    done with api call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; 
+import { HttpModule } from '@angular/http';
+import { IhcharactersComponent } from './ihcharacters/ihcharacters.component';
 
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  {path: 'characters', component: IhcharactersComponent}
+];
 
 import { AppComponent } from './app.component';
+
+import { IhService } from './services/ih.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IhcharactersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [IhService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
